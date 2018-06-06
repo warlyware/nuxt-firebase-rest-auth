2cd68ba53efe6bbd87b3ea743253f13cf5eaf5a8
Daniel Ward <danward@gmail.com>
2018-06-06 15:34:59 -0400

    Add AppHeader with login and logout buttons that correspond with current user

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,15 +1,26 @@
 <template>
   <header>
-    <button @click="handleLogout">Logout</button>
+    <button @click="handleLogout" v-if="authenticated">Logout</button>
+    <nuxt-link tag="button" to="/auth" v-else>Login</nuxt-link>
   </header>
 </template>
 
 <script>
 export default {
+  asyncData({}) {
+    return {
+      authenticated: store.getters['auth/isAuthenticated']
+    }
+  },
   methods: {
     handleLogout() {
       this.$store.dispatch('auth/logout');
       this.$router.push('/');
+    }
+  },
+  computed: {
+    authenticated() {
+      return this.$store.getters['auth/isAuthenticated'];
     }
   }
 }
